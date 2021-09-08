d8149123fc2bb97aacfdfd8e8210b93bfb0dfc31
kevinmsouza <kevinm.souza@hotmail.com>
2021-09-08 03:15:39 -0400

    Adicionado funcionalidade a barra lateral

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,13 +13,16 @@ function enterChat(){
     username = document.querySelector(".username").value;
     const promise = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants', {name: username});
     promise.then( function (response){
-        keepConectionKey = setInterval(keepConection, 1000);
+        keepConectionKey = setInterval(keepConection, 3000);
         document.querySelector(".login").classList.add("hidden")
     });
     promise.catch( function(error){
         alert("Nome Invalido ou já existente")
          
     });
+    setInterval(getParticipants, 10000);
+    document.addEventListener("keyup", sendWithEnter);
+    attFooter();
 }
 
 function keepConection(){
@@ -51,7 +54,7 @@ function formatMessages(response) {
                 </div>`);
                 break;        
             case "private_message":
-                if (response.data[i].to === username || response.data[i].from === username) {
+                if (response.data[i].to === username || response.data[i].from === username || response.data[i].to === "Todos") {
                     messages.push(`<div class="message pinkBg">
                     <p><time>${response.data[i].time}</time>  <strong>${response.data[i].from}</strong> reservadamente para <strong>${response.data[i].to}</strong>:  ${response.data[i].text} </p>
                 </div>`);
@@ -85,9 +88,9 @@ function sendMessage(){
     if (document.querySelector("input").value !== "") {
         const message = {
             from: username,
-            to: "Todos",
+            to: msgto,
             text: document.querySelector("input").value,
-            type: "message"
+            type: `${isPrivate ? "private_message": "message"}`
         } 
         document.querySelector("input").value = "";
         const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages", message)
@@ -108,19 +111,50 @@ function renderParticipants(response) {
     const participantBox = document.querySelectorAll(".itens-box")[1]
     const data = response.data.map((participant) => participant.name)
     const selectedIndex = data.indexOf(msgto);
+    console.log(selectedIndex)
     participantBox.innerHTML = `<h5>Escolha um contato <br>
     para enviar mensagem:</h5>
-    <div class="aside-item ${selectedIndex < 0 ? "selected":""}">
+    <div class="aside-item ${selectedIndex < 0 ? "selected":""}" onclick="changeMsgto(this)">
         <ion-icon name="people" ></ion-icon>
         <p>Todos</p>
         <ion-icon class="checkmark" name="checkmark"></ion-icon>
     </div>`
     data.forEach((name, i) => {
-        participantBox.innerHTML += `<div class="aside-item ${i + 1 === selectedIndex ? "selected":""}">
+        participantBox.innerHTML += `<div class="aside-item ${i === selectedIndex ? "selected":""}" onclick="changeMsgto(this)">
             <ion-icon name="person-circle"></ion-icon>
             <p>${name}</p>
             <ion-icon class="checkmark" name="checkmark"></ion-icon>
         </div>`
     });
+}
+function changePrivacy(element) {
+    isPrivate = element.children[1].innerText === "Público" ? false : true ;
+    const privacyBox = document.querySelectorAll(".itens-box")[0]
+    privacyBox.innerHTML= `<h5>Escolha a visibilidade:</h5>
+    <div class="aside-item ${isPrivate? "" : "selected"}" onclick="changePrivacy(this)" >
+        <ion-icon name="lock-open" ></ion-icon>
+        <p>Público</p>
+        <ion-icon class="checkmark" name="checkmark"></ion-icon>
+    </div>
+    <div class="aside-item ${isPrivate? "selected" : ""}" onclick="changePrivacy(this)">
+        <ion-icon name="lock-closed" ></ion-icon>
+        <p>Reservadamente</p>
+        <ion-icon class="checkmark" name="checkmark"></ion-icon>
+    </div>`
+    attFooter()
+}
+function changeMsgto(element) {
+    msgto = element.children[1].innerText;
+    getParticipants();
+    attFooter()
+}
+function sendWithEnter(event){
+    if(event.key === "Enter"){
+        sendMessage()
+    }
+}
+function attFooter(){
+    document.querySelector("footer p").innerHTML = `<p>Enviando para ${msgto} ${isPrivate? "(reservadamente)" : ""}</p>`
+    
 
 }
